b6b588edfab52ca6e80a2de1bbdfa167d34bb387
JosephMMS <josephzx1@hotmail.com>
2021-01-14 11:36:14 -0600

    Secuencia de numeros

--- index.js
+++ index.js
@@ -8,15 +8,32 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
     constructor() {
         this.inicializar();
-        
+        this.generarSecuencia();
+
     }
 
     inicializar() {
         //De esta manera agregamos clases css en el DOM
-        btnEmpezar.classList.add('hide')
+        btnEmpezar.classList.add('hide');
+        this.nivel = 1;
+        //En los casos que las variables se llamen igual a los campos que se van a asignar, javascript permite hacerlo de la siguiente manera:
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+    generarSecuencia() {
+        /**
+         * .fill llena el array de 0, una ves que tenemos numero definido
+         * map () se encarga de cambiar esos 0 por numeros random(randon retona valores entre 0 y 1)
+         * math.floor redondea hacia abajo los numeros 
+         *  */
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
     }
 }
 
 function empezarJuego() {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
