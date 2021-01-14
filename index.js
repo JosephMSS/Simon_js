b0cdb0561f6fa11d580d9dabd057bd961f82dcff
JosephMMS <josephzx1@hotmail.com>
2021-01-14 13:25:33 -0600

    Verificacion de eventos

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
+const ULTIMO_NIVEL = 10;
 
 class Juego {
     constructor() {
         this.inicializar();
         this.generarSecuencia();
-        this.siguienteNivel();
+        setTimeout(()=>{
+            this.siguienteNivel();
+        },500);
 
     }
 
     inicializar() {
+        this.elegirColor = this.elegirColor.bind(this);
+        this.siguienteNivel=this.siguienteNivel.bind(this);
         //De esta manera agregamos clases css en el DOM
         btnEmpezar.classList.add('hide');
         this.nivel = 1;
@@ -32,10 +37,12 @@ class Juego {
          * map () se encarga de cambiar esos 0 por numeros random(randon retona valores entre 0 y 1)
          * math.floor redondea hacia abajo los numeros 
          *  */
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4));
     }
-    siguienteNivel() { 
+    siguienteNivel() {
+        this.subnivel = 0;
         this.iluminarSecuencia();
+        this.agregarEventosClick()
     }
     transformarNumeroAColor(numero) {
         switch (numero) {
@@ -47,6 +54,21 @@ class Juego {
                 return 'naranja';
             case 3:
                 return 'verde';
+
+            default:
+                break;
+        }
+    }
+    transformarColorANumero(color) {
+        switch (color) {
+            case 'celeste':
+                return 0;
+            case 'violeta':
+                return 1;
+            case 'naranja':
+                return 2;
+            case 'verde':
+                return 3;
 
             default:
                 break;
@@ -77,6 +99,55 @@ class Juego {
     apagarColor(color) {
         this.colores[color].classList.remove('light');
     }
+    agregarEventosClick() {
+        /**
+         * Javascript puede perder el contexto del this, sin embargo nosotrso 
+         * podemos decirle a quien  se refiere con bind )
+         */
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+    }
+    eliminarEventosClick(){
+        this.colores.celeste.removeEventListener('click', this.elegirColor)
+        this.colores.violeta.removeEventListener('click', this.elegirColor)
+        this.colores.naranja.removeEventListener('click', this.elegirColor)
+        this.colores.verde.removeEventListener('click', this.elegirColor)
+    }
+    elegirColor(ev) {
+        /**
+         * Si no enviamos usamos el bind, el this.ev se va a referir a window, y si lo empleamos 
+         * se va a referir a la instancia del objeto juego.
+         * console.log(this);
+         */
+        /**
+         * en los datos del target
+         * se encuentran los dataset, estos los definimos en el html con data-nombre_atributo
+         */
+        // console.log(ev);
+        const nombreColor = ev.target.dataset.color;
+        const numeroColor = this.transformarColorANumero(nombreColor);
+        this.iluminarColor(nombreColor);
+        if (numeroColor === this.secuencia[this.subnivel]) {
+            this.subnivel++;
+            if (this.subnivel === this.nivel) {
+                this.nivel++;
+                this.eliminarEventosClick();
+                if (this.nivel === (ULTIMO_NIVEL + 1)) {
+                    //gano
+                } else {
+                    //Se hace refencia a la funcion , no se esta invocando (siguienteNivel())
+                   setTimeout( this.siguienteNivel,1500);
+                }
+            }
+        } else {
+            ///perdio
+        }
+
+
+    }
+
 }
 
 function empezarJuego() {
